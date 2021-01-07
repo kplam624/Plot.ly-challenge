91090c73b688f26507855b3256a703aab177c6fb
kplam624 <kplam624@gmail.com>
2021-01-07 17:54:33 -0500

    Condensed the init() and created a new function.

--- Plots/static/js/app.js
+++ Plots/static/js/app.js
@@ -73,15 +73,10 @@ function buildCharts(sample) {
     
 };
 
-// Define function that will run on page load
-function init() {
-
-    // Selecting the tags
-    var panelBox = d3.select(".panel-primary");
-
+function buildDropdown(){
     // Read json data
     d3.json("././samples.json").then(function(data){
-    
+
         // Parse and filter data to get sample names
         var name = data.names;
         var meta = data.metadata;
@@ -91,72 +86,17 @@ function init() {
         Object.entries(name).forEach(function([key,value]){
             dropdown.append("option").text(value);
         });
-
-    // Use first sample to build metadata and initial plots
-        var filteredMeta = meta.filter(subject => subject.id === 940);
-
-        Object.entries(filteredMeta).forEach(function([key,value]){
-            Object.entries(value).forEach(function([key2,value2]){
-                var panel = panelBox.append("div").attr("id","sample-metadata").attr("class","panel-body");
-                panel.text(key2 + ":" + value2);
-            });
-            panelBox.select(".panel-body").remove();
-        });  
-        
-        // Pull the data to create plots
-        var sample = data.samples;
-
-        // Filter the plot to find id.
-        var person = sample.filter(subject => subject.id === "940")[0];
-        console.log(person);
-        
-        // First plot will be the bar plot.
-        // Taking only the needed values for the plots
-        var topTenSample = person.sample_values.slice(0,10);
-        var topTenLabel = person.otu_labels.slice(0,10);
-        var topTenID = person.otu_ids.slice(0,10);
-        var otuId = topTenID.map(i => "OTU " + i);
-
-        // Creating the trace
-        trace = {
-                x: topTenSample.reverse(),
-                y: otuId.reverse(),
-                text : topTenLabel.reverse(),
-                type : "bar",
-                orientation: "h"
-            };
-        
-        // Creating the data for the plot
-        data = [trace];
-        
-        // Adding the layout
-        layout = {
-        title: "Belly Button Data"
-        };
-
-        // Creating the plot where needed.
-        Plotly.newPlot("bar",data,layout);
-        
-        // Create bubble chart in correct location
-        trace2 = {
-            x: person.otu_ids,
-            y: person.sample_values,
-            mode: "markers",
-            text: person.otu_labels,
-            marker : {
-                size: person.sample_values,
-                color: person.otu_ids,
-            }
-        }
-
-        data2 = [trace2];
-
-        layout2 = {
-            title: "Bubble Chart"
-        };
-
-        Plotly.newPlot("bubble",data2,layout2);
     });
+};
+
+// Define function that will run on page load
+function init() {
+    
+    buildDropdown();
+    buildMetadata("940");
+    buildCharts("940");
+    var panelBox = d3.select(".panel-primary");
+    panelBox.select(".panel-body").remove();
 };
 
 function optionChanged(newSample){
